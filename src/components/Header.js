c81491f4cd29eac255ab54c9aa806c68be66e665
Ariet Sagyndykov <theartsgn@gmail.com>
2024-06-05 00:20:09 +0600

    finished

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Угадай слово</h1>
+      <p>Угадай слово, иначе тебя повесят</p>
+    </div>
+  );
+};
+
+export default Header;
